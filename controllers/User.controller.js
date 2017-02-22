e5aee746adc12e3393c66b2215718ec8c135099e
nathan <nathan.nakhjavani@gmail.com>
2017-02-22 05:24:37 -0800

    added features

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -1,21 +1,25 @@
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/User.model');
-exports.register = function (req,res){
 
-var newUser = new User();
+exports.register = function(req, res) {
+    var newUser = new User();
 
-newUser.username = req.body.username;
-newUser.email = req.body.email;
-newUser.password = req.body.password;
+    newUser.username = req.body.username;
+    newUser.email = req.body.email;
 
-newUser.save(function(err,user){
-if(err){
-console.log(err.message);
-console.log(err.errors.username.message);
+    newUser.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(8), null);
 
-res.send('err resiseting user');
-}else{
-	console.log(user);
-	res.redirect('/signup.html')
-}
-});
+    newUser.save(function(err, user) {
+        if (err) {
+            console.log("Error", err);
+            console.log("User", user)
+
+            // if username or email is already taken go to another page, for now i am just going back to the sam epage
+            res.redirect('/');
+        } else {
+            console.log(user);
+            res.redirect('/signup.html')
+        }
+    });
 };
+
--- models/User.model.js
+++ models/User.model.js
@@ -2,29 +2,29 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var mongooseHidden = require('mongoose-hidden')();
 
-var minlength = [3,'The value of path `{PATH}`(`{VALUE}`) is shorter than the minimum allowed length({minlength})'];
+var minlength = [3, 'The value of path `{PATH}`(`{VALUE}`) is shorter than the minimum allowed length({minlength})'];
 
 var UserSchema = new Schema({
 
-username: {
-	type: String,
-	minlength: minlength,
-	required:true
-},
-email:{
- type: String,
- required: true,
- unique:true
-},
-password:{
-type:String,
-hide: true
-}
+    username: {
+        type: String,
+        minlength: minlength,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        hide: true
+    }
 });
 
 UserSchema.plugin(mongooseHidden);
 
-module.exports = mongoose.model('User',UserSchema);
+module.exports = mongoose.model('User', UserSchema);
 
 //ToDo
 // make sure app works when usernaem is repetititve
